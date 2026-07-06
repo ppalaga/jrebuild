97094f4257314a85620d2ec6756160e3a333533a
Peter Palaga <ppalaga@redhat.com>
2026-07-06 17:31:10 +0200

    Non-Blocking LocalMavenRepository

---
+++ core/src/main/java/org/l2x6/jrebuild/core/maven/LocalMavenRepository.java
@@ -0,0 +1,107 @@
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2025 jrebuild project contributors as indicated by the @author tags
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package org.l2x6.jrebuild.core.maven;
+
+import io.smallrye.mutiny.Multi;
+import io.vertx.mutiny.core.file.FileSystem;
+import java.nio.file.Path;
+import java.util.Iterator;
+import org.l2x6.pom.tuner.model.Gav;
+import org.l2x6.pom.tuner.model.Gavtc.Type;
+import org.l2x6.pom.tuner.model.Gavtcf;
+
+public record LocalMavenRepository(Path rootDirectory, FileSystem fileSystem) {
+
+    public Multi<Gavtcf> gavtcfStream() {
+        return walkDirectories(rootDirectory.toString())
+                .onItem().transformToMultiAndMerge(dirPath -> VersionDirectory.of(rootDirectory, Path.of(dirPath), fileSystem))
+                .onItem().transformToMultiAndMerge(VersionDirectory::artifacts);
+    }
+
+    private Multi<String> walkDirectories(String dir) {
+        return fileSystem.readDir(dir)
+                .onItem().transformToMulti(files -> Multi.createFrom().iterable(files))
+                .onItem().transformToMultiAndMerge(file -> fileSystem.props(file)
+                        .onItem().transformToMulti(props -> {
+                            if (props.isDirectory()) {
+                                return Multi.createBy().merging().streams(
+                                        Multi.createFrom().item(file),
+                                        walkDirectories(file));
+                            }
+                            return Multi.createFrom().empty();
+                        }));
+    }
+
+    static class VersionDirectory {
+
+        static Multi<VersionDirectory> of(Path rootDirectory, Path versionDirectory, FileSystem fileSystem) {
+            final Path relDir = rootDirectory.relativize(versionDirectory);
+            String version = relDir.getFileName().toString();
+            final Path artifactDir = relDir.getParent();
+            if (artifactDir == null) {
+                return Multi.createFrom().empty();
+            }
+            String artifactId = artifactDir.getFileName().toString();
+            final Path groupDir = artifactDir.getParent();
+            if (groupDir == null) {
+                return Multi.createFrom().empty();
+            }
+            String pomFile = versionDirectory.resolve(artifactId + "-" + version + ".pom").toString();
+            return fileSystem.exists(pomFile)
+                    .onItem().transformToMulti(exists -> {
+                        if (!exists) {
+                            return Multi.createFrom().empty();
+                        }
+                        Iterator<Path> it = groupDir.iterator();
+                        final StringBuilder groupId = new StringBuilder(it.next().toString());
+                        while (it.hasNext()) {
+                            groupId.append('.').append(it.next().toString());
+                        }
+                        return Multi.createFrom().item(
+                                new VersionDirectory(
+                                        versionDirectory,
+                                        new Gav(groupId.toString(), artifactId, version),
+                                        fileSystem));
+                    });
+        }
+
+        private VersionDirectory(Path absPath, Gav gav, FileSystem fileSystem) {
+            this.absVersionDir = absPath;
+            this.gav = gav;
+            this.fileSystem = fileSystem;
+        }
+
+        private final Path absVersionDir;
+        private final Gav gav;
+        private final FileSystem fileSystem;
+
+        public Multi<Gavtcf> artifacts() {
+            String prefix = gav.getArtifactId() + "-" + gav.getVersion();
+            return fileSystem.readDir(absVersionDir.toString())
+                    .onItem().transformToMulti(files -> Multi.createFrom().iterable(files))
+                    .onItem().transform(Path::of)
+                    .select().where(file -> {
+                        String fileName = file.getFileName().toString();
+                        return fileName.startsWith(prefix)
+                                && !fileName.endsWith(".asc")
+                                && !fileName.endsWith(".md5")
+                                && !fileName.endsWith(".sha1")
+                                && !fileName.endsWith(".sha256")
+                                && !fileName.endsWith(".sha512");
+                    })
+                    .onItem().transform(file -> {
+                        String fileName = file.getFileName().toString();
+                        final int lastPeriodPos = fileName.lastIndexOf('.');
+                        final String type = fileName.substring(lastPeriodPos + 1);
+                        final String classifier = (prefix.length() == lastPeriodPos)
+                                ? null
+                                : fileName.substring(prefix.length() + 1, lastPeriodPos);
+                        return gav.toGavtc(Type.of(type), classifier)
+                                .toGavtcf(absVersionDir.resolve(file.getFileName()));
+                    });
+        }
+    }
+
+}
---
+++ core/src/test/java/org/l2x6/jrebuild/core/maven/LocalMavenRepositoryTest.java
@@ -0,0 +1,50 @@
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2025 jrebuild project contributors as indicated by the @author tags
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package org.l2x6.jrebuild.core.maven;
+
+import io.vertx.mutiny.core.Vertx;
+import io.vertx.mutiny.core.file.FileSystem;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.l2x6.pom.tuner.model.Gav;
+import org.l2x6.pom.tuner.model.Gavtc.Type;
+import org.l2x6.pom.tuner.model.Gavtcf;
+
+public class LocalMavenRepositoryTest {
+    @Test
+    void local() {
+        Path root = Paths.get("target/test-classes/LocalMavenRepositoryTest").toAbsolutePath().normalize();
+        io.vertx.core.Vertx coreVertx = io.vertx.core.Vertx.vertx();
+        final Vertx vertx = new Vertx(coreVertx);
+        FileSystem fs = vertx.fileSystem();
+        LocalMavenRepository repo = new LocalMavenRepository(root, fs);
+        List<Gavtcf> found = repo.gavtcfStream().collect().asList().await().indefinitely();
+        Gav pt490 = Gav.of("org.l2x6.pom-tuner:pom-tuner:4.9.0");
+        Gav pt4100 = Gav.of("org.l2x6.pom-tuner:pom-tuner:4.10.0");
+        Assertions.assertThat(found).containsExactlyInAnyOrder(
+                Gav.of("org.l2x6.pom-tuner:pom-tuner-parent:4.9.0").toGavtc(Type.pom(), null)
+                        .toGavtcf(root.resolve("org/l2x6/pom-tuner/pom-tuner-parent/4.9.0/pom-tuner-parent-4.9.0.pom")),
+                pt490.toGavtc(Type.pom(), null)
+                        .toGavtcf(root.resolve("org/l2x6/pom-tuner/pom-tuner/4.9.0/pom-tuner-4.9.0.pom")),
+                pt490.toGavtc(Type.jar(), null)
+                        .toGavtcf(root.resolve("org/l2x6/pom-tuner/pom-tuner/4.9.0/pom-tuner-4.9.0.jar")),
+                pt490.toGavtc(Type.jar(), "sources")
+                        .toGavtcf(root.resolve("org/l2x6/pom-tuner/pom-tuner/4.9.0/pom-tuner-4.9.0-sources.jar")),
+
+                Gav.of("org.l2x6.pom-tuner:pom-tuner-parent:4.10.0").toGavtc(Type.pom(), null)
+                        .toGavtcf(root.resolve("org/l2x6/pom-tuner/pom-tuner-parent/4.10.0/pom-tuner-parent-4.10.0.pom")),
+                pt4100.toGavtc(Type.pom(), null)
+                        .toGavtcf(root.resolve("org/l2x6/pom-tuner/pom-tuner/4.10.0/pom-tuner-4.10.0.pom")),
+                pt4100.toGavtc(Type.jar(), null)
+                        .toGavtcf(root.resolve("org/l2x6/pom-tuner/pom-tuner/4.10.0/pom-tuner-4.10.0.jar")),
+                pt4100.toGavtc(Type.jar(), "sources")
+                        .toGavtcf(root.resolve("org/l2x6/pom-tuner/pom-tuner/4.10.0/pom-tuner-4.10.0-sources.jar")),
+                pt4100.toGavtc(Type.jar(), "javadoc")
+                        .toGavtcf(root.resolve("org/l2x6/pom-tuner/pom-tuner/4.10.0/pom-tuner-4.10.0-javadoc.jar")));
+    }
+}
