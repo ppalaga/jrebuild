a42cfa9cbf31d3aaf4c0269eb6b41eebbac3ae1f
Peter Palaga <ppalaga@redhat.com>
2026-07-06 17:27:41 +0200

    Move ReferenceMavenRepository to org.l2x6.jrebuild.core.maven

--- core/src/test/java/org/l2x6/jrebuild/core/maven/ReferenceMavenRepositoryTest.java
+++ core/src/test/java/org/l2x6/jrebuild/core/maven/ReferenceMavenRepositoryTest.java
@@ -2,7 +2,7 @@
  * SPDX-FileCopyrightText: Copyright (c) 2025 jrebuild project contributors as indicated by the @author tags
  * SPDX-License-Identifier: Apache-2.0
  */
-package org.l2x6.jrebuild.core.build;
+package org.l2x6.jrebuild.core.maven;
 
 import io.vertx.core.http.HttpServer;
 import io.vertx.mutiny.core.Vertx;
